696c6da3c38a9c9ffc5579d09f3381f7d9d50089
ryan quinn <ryanwilliamquinn@gmail.com>
2016-06-15 15:52:04 -0400

    use forever, hookup travis to code deploy

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+sudo /etc/init.d/nginx start
+
+npm i -g forever
+
 su ec2-user
 cd /home/ec2-user
-touch iwashere
+
 npm install
-node server.js
-sudo /etc/init.d/nginx start
+forever start server.js
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo pkill -9 node
+forever stopall
