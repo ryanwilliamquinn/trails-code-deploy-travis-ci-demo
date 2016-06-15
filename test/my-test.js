be0a70440adb580e53eb4fcf60fed60e446302ec
ryan quinn <ryanwilliamquinn@gmail.com>
2016-06-14 22:03:39 -0400

    initial repository setup

---
+++ test/my-test.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const assert = require('assert')
+
+describe('stand-in tests to explore how code deploy and travis ci work together', () => {
+  it('should do test stuff', () => {
+    assert(true)
+  })
+})
